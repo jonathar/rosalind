bbec781fe8ad8d6c32989affff1316be364669bb
Jonathan Randall <jonathar@gmail.com>
2014-07-14 22:11:14 -0600

    Solution for rosalind problem #2 - transcribe dna to rna

--- rosalind/core.py
+++ rosalind/core.py
@@ -27,3 +27,16 @@ def count_nucleotides(str):
         frequencies[nucleotide] += 1
 
     return '%(A)s %(C)s %(G)s %(T)s' % frequencies
+
+def transcribe_dna_to_rna(str):
+    '''An RNA string is a string formed from the alphabet containing 'A', 'C',
+    'G', and 'U'.
+
+    Given a DNA string t corresponding to a coding strand, its transcribed RNA
+    string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+    Given: A DNA string t having length at most 1000 nt.
+
+    Return: The transcribed RNA string of t.
+    '''
+    return str.replace('T', 'U')
