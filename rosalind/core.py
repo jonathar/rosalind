3e85d3f5666317b1e05276e4b80e2b16c8251ae6
Jonathan Randall <jonathar@gmail.com>
2014-07-27 12:34:57 -0600

    Adds solution for rosalind problem #4

--- rosalind/core.py
+++ rosalind/core.py
@@ -56,3 +56,59 @@ def reverse_compliment(dna_str):
     '''
     compliments = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join([compliments[c] for c in dna_str[::-1] if c in compliments])
+
+
+def fib_with_k(input_str):
+    """
+    Given: Positive integers n<=40 and k<=5.
+
+    Return: The total number of rabbit pairs that will be present after n
+    months if we begin with 1 pair and in each generation, every pair of
+    reproduction-age rabbits produces a litter of k rabbit pairs (instead of
+    only 1 pair).
+
+    Base case with n=1, k=3
+    >>> fib_with_k('1 3')
+    1
+
+    Secondary Base case with n=2, k=3
+    >>> fib_with_k('2 3')
+    1
+
+    First result without hitting base case
+    >>> fib_with_k('3 3')
+    4
+
+    Test n < 0 returns 0
+    >>> fib_with_k('-1 3')
+    0
+
+    Test k < 0 returns 0
+    >>> fib_with_k('3 -2')
+    0
+
+    Test with real input
+    >>> fib_with_k('4 3')
+    7
+
+    Test wtih real input
+    >>> fib_with_k('5 3')
+    19
+
+    Test with large real input
+    >>> fib_with_k('30 3')
+    20444528200
+    """
+    inputs = input_str.split(' ')
+    n = int(inputs[0])
+    k = int(inputs[1])
+
+    def fib(n, k):
+        if n < 0 or k < 0 or n == 0:
+            return 0
+        elif n ==1:
+            return 1
+        else:
+            return fib(n=n-1, k=k) + (fib(n=n - 2, k=k) * k)
+
+    return fib(n=n, k=k)
